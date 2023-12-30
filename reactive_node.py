04f8a0054cb5fec566f8e1c06ca71b01e0e271d7
Jeffrey Tan <jeftan123@gmail.com>
2023-12-30 10:35:35 -0800

    Update reactive_node.py
    -fixed steering angle grabbing the wrong index
    -reduced the range of vision so that it does not go backward but could be fixed by grabbing the gap in front of it
    -need to fix find_best_point as it is not grabbing the middle of the gap

--- reactive_node.py
+++ reactive_node.py
@@ -22,14 +22,35 @@ class ReactiveFollowGap(Node):
         # TODO: Publish to drive
         self.pub_drive = self.create_publisher(AckermannDriveStamped, drive_topic, 1)
 
+        self.bubble_dist = 2.0
+
     def preprocess_lidar(self, ranges):
         """ Preprocess the LiDAR scan array. Expert implementation includes:
             1.Setting each value to the mean over some window
             2.Rejecting high values (eg. > 3m)
         """
-        ranges[ranges >= 3] = 3
+        ranges[ranges >= self.bubble_dist] = self.bubble_dist
 
         return ranges
+    
+    def get_angle_index(self, angle_data, angle):
+        """
+        Simple helper to return the corresponding range measurement at a given angle. 
+
+        Args:
+            angle_data: single angle array from the LiDAR
+            angle: desiered angle
+
+        Returns:
+            index of desired angle
+
+        """
+        # As the angle in angle_data are not perfect, we can only choose the angles closest to what we want
+        diff_array = np.absolute(angle_data - angle) #calculates difference of every value in angle_data to angle
+        np.absolute(diff_array) # Calculates the absolute value element-wise so that negatives are not the min
+        index = diff_array.argmin() #picks index of lowest value
+
+        return index 
 
     def find_max_gap(self, free_space_ranges):
         """ Return the start index & end index of the max gap in free_space_ranges
@@ -39,17 +60,17 @@ class ReactiveFollowGap(Node):
         for i in range(len(free_space_ranges)-1):
             j = i+1
             
-            if free_space_ranges[0] == 3.0:
+            if free_space_ranges[0] == self.bubble_dist:
                 pass
-            elif free_space_ranges[i] != 3.0 and free_space_ranges[j] == 3.0:
+            elif free_space_ranges[i] != self.bubble_dist and free_space_ranges[j] == self.bubble_dist:
                 temp[0] = j
-            elif free_space_ranges[i] == 3.0 and free_space_ranges[j] == 3.0:
+            elif free_space_ranges[i] == self.bubble_dist and free_space_ranges[j] == self.bubble_dist:
                 pass
-            elif free_space_ranges[i] == 3.0 and free_space_ranges[j] != 3.0:
+            elif free_space_ranges[i] == self.bubble_dist and free_space_ranges[j] != self.bubble_dist:
                 temp[1] = i
                 if temp[1]-temp[0] > max_gap_index[1]-max_gap_index[0]:
                     max_gap_index = temp
-            elif free_space_ranges[-1] == 3.0:
+            elif free_space_ranges[-1] == self.bubble_dist:
                 temp[1] = j
                 if temp[1]-temp[0] > max_gap_index[1]-max_gap_index[0]:
                     max_gap_index = temp
@@ -88,24 +109,37 @@ class ReactiveFollowGap(Node):
         
         proc_ranges = self.preprocess_lidar(ranges)
         
+        #limit lidar ranges 
+        index_1 = self.get_angle_index(angles, 90 * np.pi/180)
+        index_2 = self.get_angle_index(angles, -90 * np.pi/180)
+
         # TODO:
         #Find closest point to LiDAR
 
         #Eliminate all points inside 'bubble' (set them to zero) 
 
         #Find max length gap 
-        max_lenght_gap_index = self.find_max_gap(proc_ranges)
+        max_lenght_gap_index = self.find_max_gap(proc_ranges[index_2:index_1])
         #Find the best point in the gap 
         best_point_index = self.find_best_point(max_lenght_gap_index[0], max_lenght_gap_index[1], ranges)
+        # velocity based on angle
+        velocity = 0.0
+        if np.abs(angles[best_point_index + index_2]) < 11 * np.pi/180:
+            velocity = 1.5 # velocity should be 1.5 m/s for 0-10 degrees
+        elif np.abs(angles[best_point_index + index_2]) < 21 * np.pi/180:
+            velocity = 1.0 # velocity should be 1.0 m/s for 10-20 degrees
+        else:
+            velocity = 0.5 # velocity should be 0.5 m/s otherwise
+        
         #Publish Drive message
         drive_msg = AckermannDriveStamped()
-        drive_msg.drive.steering_angle = angles[best_point_index]
+        drive_msg.drive.steering_angle = angles[best_point_index + index_2]
         drive_msg.drive.steering_angle_velocity = 0.0 #Zero means change the steering angle as quickly as possible
-        drive_msg.drive.speed = 2.0 
+        drive_msg.drive.speed = velocity
         self.pub_drive.publish(drive_msg)
 
         print("stuff")
-        print(best_point_index)
+        print(angles[best_point_index + index_2])
         
 
 def main(args=None):
